fd4dbb6ec72117f4e9f203e1824129c78cf662ba
sachin supunthaka <sachinsupunthaka96@gmail.com>
2023-11-29 20:20:40 +0530

    chor: multidex and firebase init

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
-import 'package:chat_app_firebase/screens/get_started_screen.dart';
+// import 'package:chat_app_firebase/screens/get_started_screen.dart';
 import 'package:chat_app_firebase/screens/home_screen.dart';
 import 'package:chat_app_firebase/screens/login_screen.dart';
 import 'package:chat_app_firebase/screens/register_screen.dart';
 // import 'package:chat_app_firebase/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
